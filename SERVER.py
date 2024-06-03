a78c758a59dfc7348c135d6fd9664eca703565dd
adityanath <adityanath5002@gmail.com>
2024-06-03 20:25:18 +0530

    remove .pem files

--- SERVER.py
+++ SERVER.py
@@ -16,13 +16,15 @@ def Create_socket():
 
 #GET-PRIVATE-KEY AND PUBLIC-KEY
 def Get_public_and_private_keys()   :
-    with open("private.pem","rb") as f:
-        private_key=rsa.PrivateKey.load_pkcs1(f.read())
+    # with open("private.pem","rb") as f:
+    #     private_key=rsa.PrivateKey.load_pkcs1(f.read())
 
-    with open("public.pem","rb") as f:
-        public_key=rsa.PublicKey.load_pkcs1(f.read())
+    # with open("public.pem","rb") as f:
+    #     public_key=rsa.PublicKey.load_pkcs1(f.read())
     
-    return public_key,private_key
+    # return public_key,private_key
+    public_key,privatekey=rsa.newkeys(1024)
+    return public_key,privatekey
 
 
 
